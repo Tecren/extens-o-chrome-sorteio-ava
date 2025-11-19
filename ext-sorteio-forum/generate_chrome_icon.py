0054e18feb6a2e8d295da0cb44653a6ed055fc2a
Guilherme C. M <gui.cmachado10@gmail.com>
2025-11-19 11:41:03 -0300

    melhoria visual

--- ext-sorteio-forum/generate_chrome_icon.py
+++ ext-sorteio-forum/generate_chrome_icon.py
@@ -1,87 +1,58 @@
 from PIL import Image, ImageDraw
-import argparse
-import os
-import math
+import argparse, os
+
+def hex_to_rgba(h):
+    h = h.lstrip('#')
+    if len(h) == 3:
+        h = ''.join([c*2 for c in h])
+    return (int(h[0:2],16), int(h[2:4],16), int(h[4:6],16), 255)
 
 
-def hex_to_rgba(hex_color):
-    hex_color = hex_color.lstrip('#')
-    if len(hex_color) == 3:
-        hex_color = ''.join([c*2 for c in hex_color])
-    r = int(hex_color[0:2], 16)
-    g = int(hex_color[2:4], 16)
-    b = int(hex_color[4:6], 16)
-    return (r, g, b, 255)
+def draw_division_icon(draw, size):
+    blue = hex_to_rgba("#44749d")
+    white = (255, 255, 255, 255)
+
+    r_outer = size * 0.48
+    draw.ellipse((size*0.5 - r_outer, size*0.5 - r_outer,
+                   size*0.5 + r_outer, size*0.5 + r_outer), fill=blue)
+
+    bar_h = size * 0.14
+    draw.rounded_rectangle(
+        (size*0.22, size*0.50 - bar_h/2, size*0.78, size*0.50 + bar_h/2),
+        radius=bar_h/2,
+        fill=white
+    )
+
+    r_dot = size * 0.14
+
+    cx_top = size * 0.41
+    cy_top = size * 0.28
+    draw.ellipse((cx_top - r_dot, cy_top - r_dot,
+                  cx_top + r_dot, cy_top + r_dot), fill=white)
+
+    cx_bottom = size * 0.59
+    cy_bottom = size * 0.72
+    draw.ellipse((cx_bottom - r_dot, cy_bottom - r_dot,
+                  cx_bottom + r_dot, cy_bottom + r_dot), fill=white)
 
 
-def draw_circles_icon(draw, size, fg):
-    r = size * 0.22
-    positions = [
-        (size*0.32, size*0.38),
-        (size*0.68, size*0.38),
-        (size*0.50, size*0.68)
-    ]
-    for x, y in positions:
-        draw.ellipse((x-r, y-r, x+r, y+r), fill=fg)
-
-
-def draw_nodes_icon(draw, size, fg):
-    r = size * 0.16
-    nodes = [
-        (size*0.30, size*0.30),
-        (size*0.70, size*0.30),
-        (size*0.50, size*0.70)
-    ]
-    for i in range(len(nodes)):
-        for j in range(i+1, len(nodes)):
-            draw.line([nodes[i], nodes[j]], fill=fg, width=int(size*0.08))
-    for x, y in nodes:
-        draw.ellipse((x-r, y-r, x+r, y+r), fill=fg)
-
-
-def draw_blocks_icon(draw, size, fg):
-    block = size * 0.22
-    coords = [
-        (size*0.18, size*0.18), (size*0.42, size*0.18), (size*0.66, size*0.18),
-        (size*0.30, size*0.58), (size*0.54, size*0.58)
-    ]
-    for x, y in coords:
-        draw.rectangle((x, y, x+block, y+block), fill=fg)
-
-
-STYLES = {
-    "circles": draw_circles_icon,
-    "nodes": draw_nodes_icon,
-    "blocks": draw_blocks_icon
-}
-
-
-def generate_icon(size, style, bg_color, fg_color, transparent, out_path):
-    img = Image.new('RGBA', (size, size), (0, 0, 0, 0) if transparent else bg_color)
+def generate(size, out):
+    img = Image.new('RGBA', (size, size), (0, 0, 0, 0))
     draw = ImageDraw.Draw(img)
-    STYLES[style](draw, size, fg_color)
-    img.save(out_path)
+    draw_division_icon(draw, size)
+    img.save(out)
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--style', default='circles', choices=['circles','nodes','blocks'])
-    parser.add_argument('--bg', default='#3f4a3c')
-    parser.add_argument('--fg', default='#01a50a')
-    parser.add_argument('--transparent', action='store_true')
-    parser.add_argument('--out', default='icons')
-    args = parser.parse_args()
+    p = argparse.ArgumentParser()
+    p.add_argument('--out', default='icons')
+    a = p.parse_args()
 
-    bg = hex_to_rgba(args.bg)
-    fg = hex_to_rgba(args.fg)
+    os.makedirs(a.out, exist_ok=True)
 
-    os.makedirs(args.out, exist_ok=True)
+    for s in [16, 48, 128]:
+        generate(s, os.path.join(a.out, f'icon_{s}.png'))
 
-    sizes = [16, 48, 128]
-    for s in sizes:
-        out_file = os.path.join(args.out, f'icon_{s}.png')
-        generate_icon(s, args.style, bg, fg, args.transparent, out_file)
-        print('Criado', out_file)
 
 if __name__ == '__main__':
     main()
